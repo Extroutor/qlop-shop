d82ea146cacb66832616d7d93c7c9c6236db64a6
E5capada <59143459+E5capada@users.noreply.github.com>
2022-10-20 17:36:08 +0300

    Update db.js
    Adding ssl for database connection

--- server/db.js
+++ server/db.js
@@ -6,6 +6,13 @@ module.exports = new Sequelize(
     process.env.DB_PASSWORD, // ПАРОЛЬ
     {
         dialect: 'postgres',
+        // Для удаленного подключения
+        dialectOptions: {
+            ssl: {
+                require: true, // This will help you. But you will see nwe error
+                rejectUnauthorized: false // This line will fix new error
+            }
+        },
         host: process.env.DB_HOST,
         port: process.env.DB_PORT
     }
