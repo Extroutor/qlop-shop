04fbc6da3b6b041fc6f443f4d5cbd3cc0f22675c
E5capada <59143459+E5capada@users.noreply.github.com>
2022-10-22 15:07:41 +0300

    Update categoryRouter.js

--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -1,7 +1,10 @@
 const Router = require('express')
 const router = new Router()
+const categoryController = require('../controllers/CategoryController')
 
-router.post('/',)
-router.get('/',)
+router.post('/', categoryController.create)
+router.get('/', categoryController.getAll)
+router.get('/:id', categoryController.getOne)
+router.delete('/:id', categoryController.delete)
 
 module.exports = router
