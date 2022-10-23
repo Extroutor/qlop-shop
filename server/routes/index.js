901668713407207ad64ac115634b2c431d68db1e
E5capada <59143459+E5capada@users.noreply.github.com>
2022-10-23 19:11:34 +0300

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,13 @@ const router = new Router()
 const userRouter = require('./userRouter')
 const ProductRouter = require('./ProductRouter')
 const categoryRouter = require('./categoryRouter')
+const basketRouter = require('./basketRouter')
+const favoriteRouter = require('./favoriteRouter')
 
 router.use('/user', userRouter)
 router.use('/category', categoryRouter)
 router.use('/product', ProductRouter)
+router.use('/basket', basketRouter)
+router.use('/favorite', favoriteRouter)
 
 module.exports = router
