f671c366c01ad17c5aaa124465b9e0ae8a8a06ce
E5capada <59143459+E5capada@users.noreply.github.com>
2022-11-21 00:32:54 +0300

    Update userRouter.js

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const userController = require ('../controllers/userController')
+const authMiddleware = require('../middleware/authmiddleware')
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.check)
+router.put('/:id', userController.update)
+router.delete('/:id', userController.delete)
+router.get('/auth', authMiddleware, userController.check)
 
 module.exports = router
