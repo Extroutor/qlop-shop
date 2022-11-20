e808ee92912b7b28726ceee8ac6210acf76ac585
E5capada <59143459+E5capada@users.noreply.github.com>
2022-11-20 03:08:08 +0300

    Update productRouter.js

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -5,7 +5,6 @@ const productController = require('../controllers/ProductController')
 router.post('/', productController.create )
 router.get('/', productController.getAll)
 router.get('/:id', productController.getOne)
-router.put('/:id', productController.update)
 router.delete('/:id', productController.delete)
 
 module.exports = router
